fe94b7191d734675ddaae75c62d92e6749b70c96
Luan Zelinski Corrêa <78267800+luanzelinskicorrea@users.noreply.github.com>
2022-06-20 10:46:13 -0300

    Add files via upload

---
+++ Java/exercicios/exercicios-apostila/listapag26luanzelinskicorrea/exerci03/exerci03.java
@@ -0,0 +1,25 @@
+//Aluno: Luan Zelinski Corrêa || Turma: 3-54
+//
+//Programa que calcule o valor das seguintes equações:
+//
+//a. 3-2-1+2+1+3
+//b. 2*3-4*5
+//c. 2+6-3/7*9
+//d. 3%4-8
+
+public class exerci03 {
+    public static void main(String[] args) {
+
+        Double a, b, c, d;
+
+        a = (double)(3-2-1+2+1+3);
+        b = (double)(2*3-4*5);
+        c = (double)(2+6-3/7*9);
+        d = (double)(3%4-8);
+
+        System.out.println("O resultado da equação A é: "+a);
+        System.out.println("O resultado da equação B é: "+b);
+        System.out.println("O resultado da equação C é: "+c);
+        System.out.println("O resultado da equação D é: "+d);
+    }
+}
